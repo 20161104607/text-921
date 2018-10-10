008513101f1ea8e185704116d3072b8ce438252a
20161104607 <a20161104607@mac26.local>
2018-10-10 11:27:56 +0800

    5
    加减乘除

--- text 921/ViewController.swift
+++ text 921/ViewController.swift
@@ -12,40 +12,105 @@ class ViewController: UIViewController {
     
     
     @IBOutlet weak var display: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    var re = 0
     
     @IBAction func zero(_ sender: Any) {
         
         display.text="0"
     }
     @IBAction func one(_ sender: Any) {
-        display.text=display.text!+"1"
+        if re == 1{
+            display.text = "1"
+        }
+        else{
+            display.text=display.text!+"1"
+            
+        }
     }
     @IBAction func two(_ sender: Any) {
-        display.text=display.text!+"2"
+        if re == 1{
+            display.text = "2"
+        }
+        else{
+            display.text=display.text!+"2"
+            
+        }
     }
 
     @IBAction func three(_ sender: Any) {
-        display.text=display.text!+"3"
+        if re == 1{
+            display.text = "3"
+        }
+        else{
+            display.text=display.text!+"3"
+            
+        }
     }
     
     @IBAction func four(_ sender: Any) {
-        display.text=display.text!+"4"
+        if re == 1{
+            display.text = "4"
+        }
+        else{
+            display.text=display.text!+"4"
+            
+        }
     }
     @IBAction func five(_ sender: Any) {
-        display.text=display.text!+"5"
+        if re == 1{
+            display.text = "5"
+        }
+        else{
+            display.text=display.text!+"5"
+            
+        }
     }
     
     @IBAction func six(_ sender: Any) {
-        display.text=display.text!+"6"
+        if re == 1{
+            display.text = "6"
+        }
+        else{
+            display.text=display.text!+"6"
+            
+        }
     }
     @IBAction func seven(_ sender: Any) {
-        display.text=display.text!+"7"
+        if re == 1{
+            display.text = "7"
+        }
+        else{
+            display.text=display.text!+"7"
+            
+        }
     }
     @IBAction func eight(_ sender: Any) {
-        display.text=display.text!+"8"
+        if re == 1{
+            display.text = "8"
+        }
+        else{
+            display.text=display.text!+"8"
+            
+        }
     }
     @IBAction func nine(_ sender: Any) {
-        display.text=display.text!+"9"
+        if re == 1{
+            display.text = "9"
+        }
+        else{
+            display.text=display.text!+"9"
+            
+        }
     }
 
     @IBAction func dump(_ sender: Any) {
@@ -68,15 +133,9 @@ class ViewController: UIViewController {
     @IBAction func equal(_ sender: Any) {
     }
     @IBAction func point(_ sender: Any) {
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        display.text = display.text! + "."
+        //judge = 1
     }
 
 
